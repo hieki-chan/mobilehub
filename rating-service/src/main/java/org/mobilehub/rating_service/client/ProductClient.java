595120632edbbec285a7c1903efcb174c53fdc2c
hieki-chan <hieuca205@gmail.com>
2025-11-23 22:16:57 +0700

    fix: rating service pom

--- rating-service/src/main/java/org/mobilehub/rating_service/client/ProductClient.java
+++ rating-service/src/main/java/org/mobilehub/rating_service/client/ProductClient.java
@@ -1,16 +1,13 @@
 package org.mobilehub.rating_service.client;
 
-import org.mobilehub.cart_service.dto.response.ProductCartResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 
-@FeignClient(name = "product-service", url = "${service.product}")
+//@FeignClient(name = "product-service", url = "${service.product}")
 public interface ProductClient {
 
-    @GetMapping("/products/carts")
-    List<ProductCartResponse> getProductsInCart(@RequestParam List<Long> productIds);
+    //@GetMapping("/products/carts")
+    //List<ProductCartResponse> getProductsInCart(@RequestParam List<Long> productIds);
 }
